526a913ada011b3d001e955d0e9de8586f3cbc76
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-08-04 13:43:44 -0700

    jk its a To Do list CRUD app now

---
+++ src/components/TextBar.tsx
@@ -0,0 +1,14 @@
+import './TextBar.css'
+
+export function TextBar() {
+
+    return(
+        <div className="textBarWrapper">
+            <form action="" className="inputBar">
+                <label htmlFor="">TO DO: </label>
+                <input type="text" name="" id="" />
+                <input type="submit" value="Add" />
+            </form>
+        </div>
+    )
+}
