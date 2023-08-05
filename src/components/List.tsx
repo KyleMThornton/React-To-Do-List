c9f887aa07661d1120c7f93f1bead82b5067bf9c
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-08-04 17:25:52 -0700

    Able to add to do items into state

---
+++ src/components/List.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export function List() {
+    const [toDo, addToDo] = useState([])
+
+    return (
+        <div className="list">
+
+        </div>
+    )
+}
--- src/components/TextBar.tsx
+++ src/components/TextBar.tsx
@@ -1,13 +1,27 @@
 import './TextBar.css'
+import { useEffect, useRef, useState } from 'react'
 
 export function TextBar() {
+    const [toDoList, setToDoList] = useState<string[]>([])
+
+    let toDoInputValue : any = useRef();
+
+    function handleSubmit(event : any) {
+        event.preventDefault();
+        setToDoList([...toDoList, toDoInputValue.current?.value])
+        toDoInputValue.current.value = ""
+    }
+
+    useEffect(() => {
+        console.log(toDoList);
+    }, [toDoList]);
 
     return(
         <div className="textBarWrapper">
             <form action="" className="inputBar">
                 <label htmlFor="">TO DO: </label>
-                <input type="text" name="" id="" />
-                <input type="submit" value="Add" />
+                <input type="text" name="toDoInput" id="toDoInput" ref={toDoInputValue} />
+                <button type="submit" onClick={handleSubmit}>Add</button>
             </form>
         </div>
     )
