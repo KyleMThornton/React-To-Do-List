13e6fecf243fc1c500bdb60e4aaf7c9f7160c871
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-08-06 01:00:36 -0700

    Added checkbox and started work on cross out

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './List.css';
 
 interface ListComponentProps {
@@ -6,9 +7,15 @@ interface ListComponentProps {
 }
 
 export const List: React.FC<ListComponentProps> = ({toDoList, deleteItem}) => {
+    const [toDoItems, setToDoItems] = useState([])
+
+    const handleCheck = (index) => {
+        
+    }
 
     const toDoListItems = toDoList.map((list:any, index:number) => 
         <div className="toDoItems" key={index}>
+            <input type="checkbox" name="taskCompleted" id="taskCompleted" />
             <p>{list}</p>
             <button onClick={() => deleteItem(index)}>X</button>
         </div>
