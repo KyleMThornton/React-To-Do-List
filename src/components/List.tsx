13d5eba71ef9a515000f781140e82d9509ae6a91
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-08-23 13:35:17 -0700

    Dndkit added but erroring

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,54 +1,87 @@
-import { useAutoAnimate } from '@formkit/auto-animate/react'
-import { DndContext, useDraggable } from '@dnd-kit/core';
+import React, { useState } from 'react';
+import {
+  DndContext,
+  closestCenter,
+  useDraggable,
+  DragEndEvent,
+} from '@dnd-kit/core';
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+  useSortable,
+} from '@dnd-kit/sortable';
 import './List.css';
-import { useState } from 'react';
+
 interface ListComponentProps {
-    toDoList: string[];
-    deleteItem: (index: number) => void;
+  toDoList: string[];
+  deleteItem: (index: number) => void;
 }
 
-export const List: React.FC<ListComponentProps> = ({toDoList, deleteItem}) => {
-    const [checkedItems, setCheckedItems] = useState<boolean[]>(new Array(toDoList.length).fill(false));
-    const [parent, enableAnimations] = useAutoAnimate();
+export const List: React.FC<ListComponentProps> = ({ toDoList, deleteItem }) => {
+  const [checkedItems, setCheckedItems] = useState<boolean[]>(
+    new Array(toDoList.length).fill(false)
+  );
 
-    const handleCheckboxChange = (index: number) => {
-        setCheckedItems(prevCheckedItems => {
-            const newCheckedItems = [...prevCheckedItems];
-            newCheckedItems[index] = !newCheckedItems[index];
-            return newCheckedItems;
-        });
-    };
+  const handleCheckboxChange = (index: number) => {
+    setCheckedItems((prevCheckedItems) => {
+      const newCheckedItems = [...prevCheckedItems];
+      newCheckedItems[index] = !newCheckedItems[index];
+      return newCheckedItems;
+    });
+  };
 
-    function handleDeleteCheck(index:number) {
-        const updatedList = checkedItems.filter((_,i) => i !== index);
-        setCheckedItems(updatedList)
-    }
+  const handleDeleteCheck = (index: number) => {
+    const updatedList = checkedItems.filter((_, i) => i !== index);
+    setCheckedItems(updatedList);
+    deleteItem(index);
+  };
 
-    const toDoListItems = toDoList.map((list:string, index:number) => {
-
-          return (
-            <div className="toDoItems" key={index}>
-                <input 
-                    type="checkbox" 
-                    name="taskCompleted" 
-                    id={`taskCompleted-${index}`} 
-                    checked={checkedItems[index]}
-                    onChange={() => handleCheckboxChange(index)}
-                />
-                <p style={{ textDecoration: checkedItems[index] ? 'line-through' : 'none' }}>
-                    {list}
-                </p>
-                <button onClick={() => {
-                    handleDeleteCheck(index)
-                    deleteItem(index)
-                }}>X</button>
-            </div>
-          )
-        });
+  const toDoListItems = toDoList.map((list: string, index: number) => {
+    const { attributes, listeners, setNodeRef, transform, transition } =
+      useSortable({ id: index });
 
     return (
-      <div className="listWrapper" ref={parent}>
-        {toDoListItems}
+      <div
+        className="toDoItems"
+        key={index}
+        ref={setNodeRef}
+        {...attributes}
+        {...listeners}
+      >
+        <input
+          type="checkbox"
+          name="taskCompleted"
+          id={`taskCompleted-${index}`}
+          checked={checkedItems[index]}
+          onChange={() => handleCheckboxChange(index)}
+        />
+        <p
+          style={{
+            textDecoration: checkedItems[index] ? 'line-through' : 'none',
+          }}
+        >
+          {list}
+        </p>
+        <button
+          onClick={() => {
+            handleDeleteCheck(index);
+          }}
+        >
+          X
+        </button>
       </div>
     );
+  });
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    console.log('Drag end called');
+  };
+
+  return (
+    <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+      <SortableContext items={toDoList} strategy={verticalListSortingStrategy}>
+        <div className="listWrapper">{toDoListItems}</div>
+      </SortableContext>
+    </DndContext>
+  );
 };
