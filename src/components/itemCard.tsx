d0305d174cc469e8e23a79f07ec68adcab83a96c
Kyle Thornton <kylemthornton@gmail.com>
2023-11-29 19:07:02 -0800

    Add checkbox for finished items

--- src/components/itemCard.tsx
+++ src/components/itemCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useSortable } from "@dnd-kit/sortable";
 import {CSS} from "@dnd-kit/utilities";
 import {RiDraggable, RiDeleteBin7Fill} from "react-icons/ri"
@@ -16,15 +17,22 @@ export default function ItemCard(props:any) {
         transition,
     }
 
+    const [isChecked, setIsChecked] = useState(false);
+
     const handleDeleteClick = () => {
         props.handleDeleteItem(props.index);
         console.log('Delete!')
     };
 
+    const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+    };
+
     return (
         <div ref={setNodeRef} style={style} className="itemContainer">
             <span className="dragHandle" {...attributes} {...listeners}><RiDraggable /></span>
-            <p>{props.id}</p>
+            <p className={isChecked ? 'crossed-out' : ''}>{props.id}</p>
+            <input type="checkbox" name="itemCheckbox" id={`checkbox-${props.id}`} className="itemCheckbox" onChange={handleCheckboxChange} />
             <button className="deleteButton" onClick={handleDeleteClick}><RiDeleteBin7Fill /></button>
         </div>
     )
